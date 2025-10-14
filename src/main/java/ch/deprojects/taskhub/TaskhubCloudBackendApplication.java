188e83edce5ee1ef6c7b2514137559e96e7d36ab
Giovanni Suter <giovanni.suter@me.com>
2025-10-14 14:35:58 +0200

    Initial commit: fresh TaskHub Cloud backend setup

---
+++ src/main/java/ch/deprojects/taskhub/TaskhubCloudBackendApplication.java
@@ -0,0 +1,13 @@
+package ch.deprojects.taskhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskhubCloudBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskhubCloudBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/deprojects/taskhub/TaskhubCloudBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.deprojects.taskhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskhubCloudBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
